272f2e94377aefa13c1cd6e1db07d2ad97aa25aa
EdisonMunera <edisonmunera72@gmail.com>
2019-09-15 20:04:53 -0500

    create database and referee controller

--- Cafsa.Web/Data/Entities/Referee.cs
+++ Cafsa.Web/Data/Entities/Referee.cs
@@ -1,13 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace Cafsa.Web.Data.Entities
 {
     public class Referee
     {
         public int Id { get; set; }
-        public User User { get; set;}
+
+        [Display(Name = "Document")]
+        [MaxLength(20, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string Document { get; set; }
+
+        [Display(Name = "First Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
+        [Required(ErrorMessage = "The field {0} is mandatory.")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Cell Phone")]
+        [MaxLength(20)]
+        public string CellPhone { get; set; }
+
+        public string Address { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+
+        public string FullNameWithDocument => $"{FirstName} {LastName} - {Document}";
+        // public User User { get; set; }
     }
 }
