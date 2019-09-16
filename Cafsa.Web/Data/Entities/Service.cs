f1abcb4e51f525c539e288c08c41a02c68553d81
EdisonMunera <edisonmunera72@gmail.com>
2019-09-15 21:22:57 -0500

    Create Service and modify Contract

---
+++ Cafsa.Web/Data/Entities/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cafsa.Web.Data.Entities
+{
+    public class Service
+    {
+        public int Id { get; set; }
+
+        public string Remarks { get; set; }
+
+        public Referee Referee { get; set; }
+
+    }
+}
