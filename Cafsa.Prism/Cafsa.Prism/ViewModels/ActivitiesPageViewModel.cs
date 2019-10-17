df7ac589c61b65d7619bdb8ef4f491b110e5a474
EdisonMunera <edisonmunera72@gmail.com>
2019-10-17 11:00:47 -0500

    deletet

--- Cafsa.Prism/Cafsa.Prism/ViewModels/ActivitiesPageViewModel.cs
+++ Cafsa.Prism/Cafsa.Prism/ViewModels/ActivitiesPageViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using Cafsa.Common.Models;
 using Prism.Navigation;
+using System.Collections.ObjectModel;
 
 namespace Cafsa.Prism.ViewModels
 {
     public class ActivitiesPageViewModel : ViewModelBase
     {
         private RefereeResponse _referee;
+        private ObservableCollection<ActivityResponse> _activities;
 
         public ActivitiesPageViewModel(
             INavigationService navigationService) : base(navigationService)
         {
             Title = "Activities";
         }
+
+        public ObservableCollection<ActivityResponse> Activities
+        {
+            get => _activities;
+            set => SetProperty(ref _activities, value);
+        }
+
+
 
         public override void OnNavigatedTo(INavigationParameters parameters)
         {
@@ -22,7 +32,7 @@ namespace Cafsa.Prism.ViewModels
                 //se trae la informacion de las activities del referee especifico
                 _referee = parameters.GetValue<RefereeResponse>("referee");
                 Title = $"Activities of: {_referee.FullName}";
-
+                Activities = new ObservableCollection<ActivityResponse>(_referee.Activities);
 
             }
         }
