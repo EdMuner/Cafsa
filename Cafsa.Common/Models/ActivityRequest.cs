05ec58035e64175de9c00470ac96a53d04008903
EdisonMunera <edisonmunera72@gmail.com>
2019-11-06 16:40:50 -0500

    ok apis for implemntation second part mobile

---
+++ Cafsa.Common/Models/ActivityRequest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Cafsa.Common.Models
+{
+    public class ActivityRequest
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Neighborhood { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public bool IsAvailable { get; set; }
+
+        public string Remarks { get; set; }
+
+        [Required]
+        public int ActivityTypeId { get; set; }
+
+        [Required]
+        public int RefereeId { get; set; }
+    }
+
+}
