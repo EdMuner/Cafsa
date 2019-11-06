d593b49612106dc63946ed1df9df7985555c3d1d
EdisonMunera <edisonmunera72@gmail.com>
2019-11-05 23:25:47 -0500

    ok detailsActivity and detailsActivityReferee in the home controller

--- Cafsa.Web/Controllers/HomeController.cs
+++ Cafsa.Web/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Cafsa.Web.Data;
+using Cafsa.Web.Helpers;
 using Cafsa.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
@@ -11,10 +13,17 @@ namespace Cafsa.Web.Controllers
     public class HomeController : Controller
     {
         private readonly DataContext _dataContext;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IConverterHelper _converterHelper;
 
-        public HomeController(DataContext dataContext)
+        public HomeController(
+            DataContext dataContext,
+            ICombosHelper combosHelper,
+            IConverterHelper converterHelper)
         {
             _dataContext = dataContext;
+            _combosHelper = combosHelper;
+            _converterHelper = converterHelper;
         }
 
         public IActionResult Index()
@@ -79,6 +88,99 @@ namespace Cafsa.Web.Controllers
 
             return View(activity);
         }
+
+        [Authorize(Roles = "Referee")]
+        public async Task<IActionResult> MyActivities()
+        {
+            var referee = await _dataContext.Referees
+                .Include(o => o.User)
+                .Include(o => o.Services)
+                .Include(o => o.Activities)
+                .ThenInclude(p => p.ActivityType)
+                .Include(o => o.Activities)
+                .ThenInclude(p => p.ActivityImages)
+                .FirstOrDefaultAsync(o => o.User.UserName.ToLower().Equals(User.Identity.Name.ToLower()));
+            if (referee == null)
+            {
+                return NotFound();
+            }
+
+            return View(referee);
+        }
+        [Authorize(Roles = "Referee")]
+        public async Task<IActionResult> AddActivity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var referee = await _dataContext.Referees.FindAsync(id.Value);
+            if (referee == null)
+            {
+                return NotFound();
+            }
+
+            var model = new ActivityViewModel
+            {
+                RefereeId = referee.Id,
+                ActivityTypes = _combosHelper.GetComboActivityTypes()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddActivity(ActivityViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var activity = await _converterHelper.ToActivityAsync(model, true);
+                _dataContext.Activities.Add(activity);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction(nameof(MyActivities));
+            }
+
+            model.ActivityTypes = _combosHelper.GetComboActivityTypes();
+            return View(model);
+        }
+
+        [Authorize(Roles = "Referee")]
+        public async Task<IActionResult> EditActivity(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var activity = await _dataContext.Activities
+                .Include(p => p.Referee)
+                .Include(p => p.ActivityType)
+                .FirstOrDefaultAsync(p => p.Id == id.Value);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+
+            var model = _converterHelper.ToActivityViewModel(activity);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditActivity(ActivityViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var activity = await _converterHelper.ToActivityAsync(model, false);
+                _dataContext.Activities.Update(activity);
+                await _dataContext.SaveChangesAsync();
+                return RedirectToAction(nameof(MyActivities));
+            }
+
+            model.ActivityTypes = _combosHelper.GetComboActivityTypes();
+            return View(model);
+        }
+
 
     }
 }
